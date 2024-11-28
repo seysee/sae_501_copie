ee89ae34e417873f25bb2ef9da94e4733b22ed78
seyma <Seyma.Tunc@etu.univ-grenoble-alpes.fr>
2024-11-28 14:48:53 +0100

    add : page createGame

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/App.css';
 import '../styles/index.css';
 import Button from '../components/_button';
-import TextInput from "../components/_textInput";
+import TextInput from '../components/_textInput';
 
 export default function Index() {
     const [isMobileDevice, setIsMobileDevice] = useState(false);
@@ -23,12 +23,8 @@ export default function Index() {
         };
     }, []);
 
-    if (!isMobileDevice) {
-        return <div className="non-mobile-message">Ce site est accessible uniquement sur mobile. 𐐘💥ඞා </div>;
-    }
-
     return (
-        <div className="All">
+        <div>
             <div className="wrapper">
                 <span><i className="fa fa-star"></i></span>
                 <span><i className="fa fa-star"></i></span>
@@ -42,39 +38,45 @@ export default function Index() {
                 <span><i className="fa fa-star"></i></span>
             </div>
 
-            <div className="box-area flex flex-col items-center justify-center">
-                <h1 className="text-6xl font-Amatic text-white mb-8">Parmi Nous</h1>
-                <div className="w-4/5 max-w-md">
-                    <div className="mb-6">
-                        <TextInput
-                            value={pseudo}
-                            onChange={(e) => setPseudo(e.target.value)}
-                            placeholder="Entrez votre pseudo"
-                            className="w-full text-white bg-black border-white rounded-lg"
-                        />
-                    </div>
-                    <Link href="/createGame" passHref>
-                        <Button
-                            label="Créer partie"
-                            onClick={() => console.log("Créer une partie")}
-                            className="w-full mb-4 bg-black text-white border-white"
-                        />
-                    </Link>
-                    <Link href="/joinGame" passHref>
-                        <Button
-                            label="Rejoindre partie"
-                            className="w-full mb-4 bg-black text-white border-white"
-                        />
-                    </Link>
-                    <Link href="/" passHref>
-                        <Button
-                            label="Règles"
-                            className="w-full bg-black text-red-500 border-red-500"
-                        />
-                    </Link>
+            {!isMobileDevice && (
+                <div className="non-mobile-message">
+                    Ce site est accessible uniquement sur mobile. 𐐘
                 </div>
-            </div>
+            )}
 
+            {isMobileDevice && (
+                <div className="box-area flex flex-col items-center justify-center">
+                    <h1 className="text-6xl font-Amatic text-white mb-8">Parmi Nous</h1>
+                    <div className="w-4/5 max-w-md">
+                        <div className="mb-6">
+                            <TextInput
+                                value={pseudo}
+                                onChange={(e) => setPseudo(e.target.value)}
+                                placeholder="Entrez votre pseudo"
+                                className="w-full text-white bg-black border-white rounded-lg"
+                            />
+                        </div>
+                        <Link href="/createGame" passHref>
+                            <Button
+                                label="Créer partie"
+                                className="w-full mb-4 bg-black text-white border-white"
+                            />
+                        </Link>
+                        <Link href="/joinGame" passHref>
+                            <Button
+                                label="Rejoindre partie"
+                                className="w-full mb-4 bg-black text-white border-white"
+                            />
+                        </Link>
+                        <Link href="/" passHref>
+                            <Button
+                                label="Règles"
+                                className="w-full bg-black text-red-500 border-red-500"
+                            />
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
