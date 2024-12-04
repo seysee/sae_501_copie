e73850e6c77ba76056155d0b7c9881319fee8571
II-SARIP-II <paris.gougne.05@gmail.com>
2024-12-04 15:39:19 +0100

    feat: style css & role -> enigma

--- src/pages/role.jsx
+++ src/pages/role.jsx
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react';
 import axios from "axios";
 import {pl} from "@faker-js/faker";
+import {router} from "next/client";
 
 export default function Role() {
+    const [isVisible, setIsVisible] = useState(false);
     const [role, setRole] = useState(null);
     const getStoredUserData = () => {
         try {
@@ -33,23 +35,44 @@ export default function Role() {
 
     }, []);
 
+    useEffect(() => {
+        const timer = setTimeout(() => setIsVisible(true), 100); // Ajoute un délai avant l'apparition.
+        return () => clearTimeout(timer); // Nettoie le timer si le composant est démonté.
+    }, []);
+
+    useEffect(()=>{
+        const timerRouterPush = setTimeout(()=> router.push('/enigma'), 10000);
+        return () => clearTimeout(timerRouterPush); // Nettoie le timer si le composant est démonté.
+    })
+
     return (
-        <div>
-            <h2>TON ROLE EST :</h2>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h2 className="font-Amatic text-xl mb-4">TON ROLE EST :</h2>
             {role === 0 ? (
-                <h1>
+                <h1
+                    className={`font-Amatic text-4xl font-bold text-green-500 transition-opacity duration-[5000ms] ${
+                        isVisible ? "opacity-100" : "opacity-0"
+                    }`}
+                >
                     ENQUÊTEUR
                 </h1>
             ) : role === 1 ? (
-                <h1>
+                <h1
+                    className={`font-Amatic text-4xl font-bold text-red-500 transition-opacity duration-[5000ms] ${
+                        isVisible ? "opacity-100" : "opacity-0"
+                    }`}
+                >
                     SABOTEUR
                 </h1>
             ) : (
-                <h1>
-                    Problème de role
+                <h1
+                    className={`font-Amatic text-4xl font-bold text-gray-500 transition-opacity duration-[5000ms] ${
+                        isVisible ? "opacity-100" : "opacity-0"
+                    }`}
+                >
+                    Problème de rôle
                 </h1>
             )}
-
         </div>
     );
 }
