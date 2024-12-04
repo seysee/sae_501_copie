11d70c1b34dc552f4606d26e5430911da56e9982
II-SARIP-II <paris.gougne.05@gmail.com>
2024-12-04 11:20:02 +0100

    feat: show role

--- src/pages/role.jsx
+++ src/pages/role.jsx
@@ -1,8 +1,32 @@
 import {useState, useEffect} from 'react';
+import axios from "axios";
 
 export default function Role() {
     const [role, setRole] = useState(null);
-    useEffect(() => {
+    const getStoredUserData = () => {
+        try {
+            const storedPlayer = sessionStorage.getItem('userData');
+            if (storedPlayer) {
+                return JSON.parse(storedPlayer);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données utilisateur:', error);
+        }
+        return null;
+    };
+    useEffect(async () => {
+        const storedPlayer = getStoredUserData();
+        try {
+            console.log(storedPlayer.id)
+
+            const response = await axios.get('/api/player', {
+                params:{id: storedPlayer.id}
+            });
+            console.log(response)
+            setRole(response.data.role);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des joueurs :', error);
+        }
 
     }, []);
 
@@ -13,9 +37,13 @@ export default function Role() {
                 <h1>
                     ENQUÊTEUR
                 </h1>
-            ) : (
+            ) : role === 1 ? (
                 <h1>
                     SABOTEUR
+                </h1>
+            ) : (
+                <h1>
+                    Problème de role
                 </h1>
             )}
 
