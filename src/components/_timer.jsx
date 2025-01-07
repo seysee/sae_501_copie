11e45357a5a217028972eca1f598e5bdba8aa379
joran-cng <caunegre.joran@gmail.com>
2025-01-07 11:35:07 +0100

    Feat : Add Camera & Timer

---
+++ src/components/_timer.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function Timer({ initialTime, onTimeUp, paused }) {
+    const [timeLeft, setTimeLeft] = useState(initialTime);
+
+    useEffect(() => {
+        if (paused || timeLeft <= 0) {
+            if (timeLeft <= 0) onTimeUp(); // Déclenchement lorsque le temps est écoulé
+            return;
+        }
+
+        const timer = setInterval(() => {
+            setTimeLeft((prev) => prev - 1);
+        }, 1000);
+
+        return () => clearInterval(timer); // Nettoyage
+    }, [timeLeft, onTimeUp, paused]);
+
+    return (
+        <div className="text-center text-xl font-bold text-white">
+            Temps restant : {timeLeft} s
+        </div>
+    );
+}
