e0633a93a79fa440c312ff0257d6a433621f427b
CallMeTrinity <anteur.ap@gmail.com>
2025-01-07 13:51:24 +0100

    feat : balance game

--- src/components/ActionQuestion.jsx
+++ src/components/ActionQuestion.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TiltDetected from "./actions/TiltDetected";
 import ShakeDetected from "./actions/ShakeDetected";
+import BalanceGame from "./actions/BalanceGame";
 
-export default function ActionQuestion({ question }) {
+export default function ActionQuestion({ question, onSuccess }) {
     const handleSuccess = (message) => {
         console.log(message); // Affiche un message de succès
         alert(message); // Optionnel : Affiche une alerte
+        onSuccess(message); // Appelle la callback de validation
     };
 
     return (
@@ -17,6 +19,9 @@ export default function ActionQuestion({ question }) {
             )}
             {question.answer === "shake_detected" && (
                 <ShakeDetected questionId={question.id} onSuccess={handleSuccess} />
+            )}
+            {question.answer === "hole_success" && (
+                <BalanceGame questionId={question.id} onSuccess={handleSuccess} />
             )}
         </div>
     );
