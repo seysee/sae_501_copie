b4976bf7525493ca3b36e8d18e413776b30ce8b5
II-SARIP-II <paris.gougne.05@gmail.com>
2025-01-09 10:31:00 +0100

    feat: no capital letters in the code

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Index() {
     }, []);
 
     const generateCode = () => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
         let code = '';
         for (let i = 0; i < 6; i++) {
             code += characters.charAt(Math.floor(Math.random() * characters.length));
