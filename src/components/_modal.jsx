80c5555ca9e24c5120c6e9e5d7e23c29c742eb27
seyma <Seyma.Tunc@etu.univ-grenoble-alpes.fr>
2025-01-13 14:13:28 +0100

    add : component modal in page vote to confirm vote

--- src/components/_modal.jsx
+++ src/components/_modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ isOpen, onClose, onConfirm, suspectName }) => {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md">
-                <h2 className="text-2xl font-bold mb-4">Confirmer votre vote</h2>
+                <h2 className="text-2xl text-black font-bold mb-4">Confirmer votre vote</h2>
                 <p className="text-gray-700 mb-6">
                     Êtes-vous sûr de vouloir voter pour <span className="font-bold">{suspectName}</span> ?
                 </p>
