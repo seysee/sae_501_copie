702cbf618ea9ae1ce8d9472c6061dab03e4aa889
II-SARIP-II <paris.gougne.05@gmail.com>
2025-01-14 11:43:20 +0100

    feat: rebase main

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -129,11 +129,13 @@ export default function handler(req, res) {
 
             socket.on('submitAnswer', async ({ sessionId, questionId, answer, playerId }) => {
                 // Récupère la session dans la BDD
+                console.log("ICI C'EST TOUT ------------------------------",sessionId, questionId, answer, playerId);
                 let sessionDb;
                 try {
                     sessionDb = await prisma.sessions.findUnique({
                         where: { id: parseInt(sessionId) },
                     });
+                    console.log("SESSION DB RECUP : ", sessionDb)
                 } catch (e) {
                     console.error("Erreur findUnique sessions :", e);
                     return;
@@ -151,6 +153,8 @@ export default function handler(req, res) {
                         where: { sessionId: parseInt(sessionId) },
                         orderBy: { id: 'asc' }, // ou tout autre critère
                     });
+                    console.log("PLAYER DB : ", players)
+
                 } catch (e) {
                     console.error("Erreur findMany players :", e);
                     return;
@@ -168,6 +172,8 @@ export default function handler(req, res) {
                         where: { id: parseInt(sessionId) },
                         data: { activePlayerIndex: newIndex },
                     });
+                    console.log("TESST PRISMA.SESSION.UPDATE")
+
                     console.log(`Prochain joueur actif (BDD) : index = ${newIndex}`);
                 } catch (e) {
                     console.error("Erreur lors de la mise à jour de l'activePlayerIndex :", e);
@@ -182,13 +188,12 @@ export default function handler(req, res) {
                 // Rediriger vers result
                 const encryptedQuestionId = encryptParam(questionId);
                 const encryptedAnswer = encryptParam(answer);
+                socket.join(sessionId);
 
                 io.to(sessionId).emit('answerSubmitted', {
                     redirectUrl: `/result?questionId=${encodeURIComponent(encryptedQuestionId)}&answer=${encodeURIComponent(encryptedAnswer)}`,
                 });
             });
-
-
 
 
             socket.on('returnHome', (sessionId) => {
