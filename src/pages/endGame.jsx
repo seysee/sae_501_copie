94a4fdb0c87f3240e87d29e6594810565be0041b
seyma <Seyma.Tunc@etu.univ-grenoble-alpes.fr>
2025-01-17 11:11:23 +0100

    add: videos of vilain marvel

--- src/pages/endGame.jsx
+++ src/pages/endGame.jsx
@@ -10,6 +10,7 @@ export default function EndGame() {
     const [isVisible, setIsVisible] = useState(false);
     const [showFooter, setShowFooter] = useState(false);
     const router = useRouter();
+    const [isExiting, setIsExiting] = useState(false);
 
     const getStoredUserData = () => {
         try {
@@ -64,7 +65,11 @@ export default function EndGame() {
 
             sessionStorage.removeItem("votedSuspectId");
 
-            router.push("/");
+            setIsExiting(true);
+
+            setTimeout(() => {
+                router.push("/");
+            }, 1000);
         } catch (error) {
             console.error("Erreur lors du nettoyage des données utilisateur :", error);
             router.push("/");
@@ -79,16 +84,25 @@ export default function EndGame() {
         3: '/videos/staline.mp4',
         4: '/videos/petain.mp4',
         5: '/videos/kimjongil.mp4',
+        6: '/videos/michael.mp4',
+        7: '/videos/freddy.mp4',
+        8: '/videos/darkvador.mp4',
+        9: '/videos/joker.mp4',
+        10: '/videos/thanos.mp4',
     };
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center p-4 text-white">
+        <div
+            className={`min-h-screen flex flex-col justify-center items-center p-4 text-white transition-opacity duration-1000 ${
+                isExiting ? "opacity-0" : "opacity-100"
+            }`}
+        >
             {error ? (
-                <p className="text-2xl font-Amatic text-red-500">{error}</p>
-            ) : (
-                suspect && (
-                    <>
-                        <div className="text-center">
+            <p className="text-2xl font-Amatic text-red-500">{error}</p>
+        ) : (
+            suspect && (
+                <>
+                <div className="text-center">
                             <p className={`text-4xl font-Amatic font-bold transition-opacity mb-6 duration-[5000ms] ${isVisible ? "opacity-100" : "opacity-0"}`}>
                                 {voteMessage}
                             </p>
